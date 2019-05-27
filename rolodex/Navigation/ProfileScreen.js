1623e3784383d9481e1945b844fba89e67b79b5f
Lloyd <lloyd.clowes@gmail.com>
2019-05-27 10:49:50 +0100

    Move all images to assets folder

--- rolodex/Navigation/ProfileScreen.js
+++ rolodex/Navigation/ProfileScreen.js
@@ -15,7 +15,7 @@ export default class ProfileScreen extends React.Component {
           </View>
           <View style={{flex:9, alignItems: "center"}} >
             <View style={{top:30}}>
-              <Image source={require('./card.png')} />
+              <Image source={require('../assets/images/card.png')} />
             </View>
         </View>
       </View>
