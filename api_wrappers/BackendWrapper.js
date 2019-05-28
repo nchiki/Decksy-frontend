9e3e84e9404c70843c1f561a432b565ff2ccf8f6
Lloyd <lloyd.clowes@gmail.com>
2019-05-28 12:29:12 +0100

    Add API Wrapper for backend

---
+++ api_wrappers/BackendWrapper.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Text, View, Platform } from 'react-native';
+
+const API = "https://rolodex.tk/"
+
+function getUserCard(userID) {
+  return fetch(`${API}/businesscards/getusercard/card`, {
+      method: 'GET',
+      body: JSON.stringify({
+        user: {userID},
+      }),
+      cache: 'default',
+    })
+    .then((response) => response.json());
+}
+
+function getUserHash(userID) {
+  return fetch(`${API}/businesscards/getusercard/hash`, {
+      method: 'GET',
+      body: JSON.stringify({
+        user: {userID},
+      }),
+      cache: 'default',
+    })
+    .then((response) => response.json());
+}
+
+function getUserHash(userID, image) {
+  return fetch(`${API}/businesscards/setusercard`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: {userID},
+        image: {image}
+      }),
+    })
+    .then((response) => response.json());
+}
+
+function getUserContacts(userID) {
+  return fetch(`${API}/user/contacts`, {
+      method: 'GET',
+      body: JSON.stringify({
+        user: {userID},
+      }),
+      cache: 'default',
+    })
+    .then((response) => response.json());
+}
+
+function getUserDetails(userID) {
+  return fetch(`${API}/user/details`, {
+      method: 'GET',
+      body: JSON.stringify({
+        user: {userID},
+      }),
+      cache: 'default',
+    })
+    .then((response) => response.json());
+}
