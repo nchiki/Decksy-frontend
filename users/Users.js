dc9352898a05b7164689b5729a767665a1fd90aa
Blanca <bt3917@ic.ac.uk>
2019-05-29 17:40:55 +0100

    merged with blanca/navigation

--- users/Users.js
+++ users/Users.js
@@ -4,77 +4,88 @@ const users=[
     company: 'Facebook',
     field: 'Software',
     phoneNumber: '073004889',
-    email:'roberts@email.com'
-    },   
+    email:'roberts@email.com',
+    color: 'darkblue'
+    },
     {
     name: 'John',
     company: 'Amazon',
     field: 'Software',
     phoneNumber: '072304889',
-    email:'john@email.com'
-    }, 
+    email:'john@email.com',
+    color: 'darkblue'
+    },
     {
     name: 'Mary',
     company: 'Google',
     field: 'Software',
     phoneNumber: '048904889',
-    email:'mary@email.com'
+    email:'mary@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Sienna',
     company: 'Facebook',
     field: 'Software',
     phoneNumber: '073004909',
-    email:'sienna@email.com'
+    email:'sienna@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Carlos',
     company: 'Palantir',
     field: 'Software',
     phoneNumber: '073007889',
-    email:'carlos@email.com'
+    email:'carlos@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Phillip',
     company: 'Facebook',
     field: 'Software',
     phoneNumber: '073004889',
-    email:'phillip@email.com'
+    email:'phillip@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Louis',
     company: 'Amazon',
     field: 'Software',
     phoneNumber: '072004889',
-    email:'louis@email.com'
+    email:'louis@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Jenna',
     company: 'Palantir',
     field: 'Software',
     phoneNumber: '073004339',
-    email:'jenna@email.com'
+    email:'jenna@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Sarah',
     company: 'Facebook',
     field: 'Software',
     phoneNumber: '073032889',
-    email:'sarah@email.com'
+    email:'sarah@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Paul',
     company: 'Amazon',
     field: 'Software',
     phoneNumber: '073007789',
-    email:'paul@email.com'
+    email:'paul@email.com',
+    color: 'darkblue'
     },
     {
     name: 'Pierre',
     company: 'Facebook',
     field: 'Software',
     phoneNumber: '073324589',
-    email:'pierre@email.com'
+    email:'pierre@email.com',
+    color: 'darkblue'
     },
     ]
 
