6740445b276ab58a105cde28138c5f93873262ba
Hubert Kaluzny <hk4817@ic.ac.uk>
2019-06-06 22:54:41 +0100

    Reformat the styles code to be prettier

--- styles/DeckStyles.js
+++ styles/DeckStyles.js
@@ -1,391 +1,344 @@
-import { StyleSheet } from 'react-native';
+const deckStyles = [
+  { // "style 2"
+    titleText: {
+      position: 'absolute',
+      top: -45,
+      left: -100,
 
-const deckStyles = {
-  getStyle2: function () {
-
-    return (
-      StyleSheet.create({
-        titleText: {
-          position: 'absolute',
-          top: -45,
-          left: -100,
-
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          right: -220,
-          bottom: -155,
-          fontSize: 35,
-          color: 'white',
-          justifyContent: 'center',
-          textAlign: 'right',
-        },
-        details: {
-          right: -185,
-          bottom: -160,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'right'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      right: -220,
+      bottom: -155,
+      fontSize: 35,
+      color: 'white',
+      justifyContent: 'center',
+      textAlign: 'right',
+    },
+    details: {
+      right: -185,
+      bottom: -160,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'right'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle3: function () {
+  { // "style 3"
+    titleText: {
+      bottom: -220,
+      right: -105,
 
-    return (
-      StyleSheet.create({
-        titleText: {
-          bottom: -220,
-          right: -105,
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'right'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      right: -200,
+      bottom: -135,
+      fontSize: 35,
+      color: 'white',
+      justifyContent: 'center',
+      textAlign: 'right'
+    },
+    details: {
+      left: -180,
+      top: -170,
+      fontSize: 30,
+      color: 'white',
 
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'right'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          right: -200,
-          bottom: -135,
-          fontSize: 35,
-          color: 'white',
-          justifyContent: 'center',
-          textAlign: 'right'
-        },
-        details: {
-          left: -180,
-          top: -170,
-          fontSize: 30,
-          color: 'white',
-
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-
-  getStyle4: function () {
-
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 35
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 45,
-          fontSize: 35,
-          color: 'white',
-          textAlign: 'center'
-        },
-        details: {
-          left: 170,
-          bottom: 150,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'right'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+  { // "style 4"
+    titleText: {
+      top: 35
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 45,
+      fontSize: 35,
+      color: 'white',
+      textAlign: 'center'
+    },
+    details: {
+      left: 170,
+      bottom: 150,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'right'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle5: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 20,
-          left: -57
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          left: -168,
-          top: 20,
-          fontSize: 35,
-          color: 'white',
+  { // "style 5"
+    titleText: {
+      top: 20,
+      left: -57
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      left: -168,
+      top: 20,
+      fontSize: 35,
+      color: 'white',
 
-        },
-        details: {
-          right: -150,
-          bottom: -30,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'right'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+    },
+    details: {
+      right: -150,
+      bottom: -30,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'right'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle6: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 55,
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 60,
-          fontSize: 35,
-          color: 'white',
-          textAlign: 'center'
+  { // "style 6"
+    titleText: {
+      top: 55,
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 60,
+      fontSize: 35,
+      color: 'white',
+      textAlign: 'center'
 
-        },
-        details: {
+    },
+    details: {
 
-          bottom: -100,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'center'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+      bottom: -100,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'center'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle7: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: -5,
-          left: -80
-        },
-        userText: {
-          textAlign: 'left',
-          fontSize: 40,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          textAlign: 'left',
-          fontWeight: 'bold',
-          left: -174,
-          top: -5,
-          fontSize: 35,
-        },
-        details: {
-          right: -155,
-          bottom: -65,
-          fontSize: 25,
-          textAlign: 'right'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+  { // "style 7"
+    titleText: {
+      top: -5,
+      left: -80
+    },
+    userText: {
+      textAlign: 'left',
+      fontSize: 40,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      textAlign: 'left',
+      fontWeight: 'bold',
+      left: -174,
+      top: -5,
+      fontSize: 35,
+    },
+    details: {
+      right: -155,
+      bottom: -65,
+      fontSize: 25,
+      textAlign: 'right'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
+  { // "style 8"
+    titleText: {
+      top: -10
+    },
+    userText: {
+      color: 'maroon',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      left: -200,
+      bottom: -139,
+      fontSize: 35,
+      fontWeight: 'bold',
+      color: 'white',
+      textAlign: 'right'
 
-  getStyle8: function () {
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: -10
-        },
-        userText: {
-          color: 'maroon',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          left: -200,
-          bottom: -139,
-          fontSize: 35,
-          fontWeight: 'bold',
-          color: 'white',
-          textAlign: 'right'
-
-        },
-        details: {
-          right: -195,
-          bottom: -95,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'right'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      }));
+    },
+    details: {
+      right: -195,
+      bottom: -95,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'right'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle9: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 35,
-          left: -50
-        },
-        userText: {
-          color: 'white',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 45,
-          left: -55,
-          fontSize: 35,
-          color: 'white',
-          textAlign: 'center'
+  { // "style 9"
+    titleText: {
+      top: 35,
+      left: -50
+    },
+    userText: {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 45,
+      left: -55,
+      fontSize: 35,
+      color: 'white',
+      textAlign: 'center'
 
-        },
-        details: {
-          left: -55,
-          bottom: -80,
-          fontSize: 25,
-          color: 'white',
-          textAlign: 'center'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+    },
+    details: {
+      left: -55,
+      bottom: -80,
+      fontSize: 25,
+      color: 'white',
+      textAlign: 'center'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle10: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 35,
-          left: -15
-        },
-        userText: {
-          color: 'lightskyblue',
-          fontSize: 40,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 45,
-          left: -15,
-          fontSize: 35,
-          color: 'lightskyblue',
-          textAlign: 'center'
+  { // "style 10"
+    titleText: {
+      top: 35,
+      left: -15
+    },
+    userText: {
+      color: 'lightskyblue',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 45,
+      left: -15,
+      fontSize: 35,
+      color: 'lightskyblue',
+      textAlign: 'center'
 
-        },
-        details: {
-          left: -190,
-          bottom: -120,
-          fontSize: 25,
-          color: 'lightskyblue',
-          textAlign: 'left'
-        },
-        links: {
-          right: 200,
-          top: 105,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
-  },
-}
+    },
+    details: {
+      left: -190,
+      bottom: -120,
+      fontSize: 25,
+      color: 'lightskyblue',
+      textAlign: 'left'
+    },
+    links: {
+      right: 200,
+      top: 105,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
+  }
+];
 
 export default deckStyles;
--- styles/TemplateStyles.js
+++ styles/TemplateStyles.js
@@ -1,322 +1,278 @@
 import { StyleSheet } from 'react-native';
 
-const templateStyles = {
-  getStyle2: function () {
-    return (
-      StyleSheet.create({
-        titleText: {
-          position: 'absolute',
-          top: 20,
-          left: 20
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          right: -85,
-          bottom: -125,
-          fontSize: 20,
-          color: 'white',
-          justifyContent: 'center',
-          textAlign: 'right'
-        },
-        details: {
-          right: -85,
-          bottom: -130,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+const templateStyles = [
+  {
+    titleText: {
+      position: 'absolute',
+      top: 20,
+      left: 20
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      right: -85,
+      bottom: -125,
+      fontSize: 20,
+      color: 'white',
+      justifyContent: 'center',
+      textAlign: 'right'
+    },
+    details: {
+      right: -85,
+      bottom: -130,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
+  {
+    titleText: {
+      bottom: -160,
+      right: -100
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'right'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      right: -110,
+      bottom: -115,
+      fontSize: 15,
+      color: 'white',
+      justifyContent: 'center',
+      textAlign: 'right'
+    },
+    details: {
+      left: -90,
+      top: -20,
+      fontSize: 10,
+      color: 'white'
+    }
+  },
+  {
+    titleText: {
+      top: 65
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 75,
+      fontSize: 15,
+      color: 'white',
+      textAlign: 'center'
+    },
+    details: {
+      right: -90,
+      top: -24,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
+  },
+  {
+    titleText: {
+      top: 65,
+      left: -60
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      left: -78,
+      top: 70,
+      fontSize: 15,
+      color: 'white',
 
-  getStyle3: function () {
-    return (
-      StyleSheet.create({
-        titleText: {
-          bottom: -160,
-          right: -100
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold',
-          textAlign: 'right'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          right: -110,
-          bottom: -115,
-          fontSize: 15,
-          color: 'white',
-          justifyContent: 'center',
-          textAlign: 'right'
-        },
-        details: {
-          left: -90,
-          top: -20,
-          fontSize: 10,
-          color: 'white'
-        }
-      })
-    );
+    },
+    details: {
+      right: -70,
+      bottom: -80,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
+  {
+    titleText: {
+      top: 55,
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 60,
+      fontSize: 15,
+      color: 'white',
+      textAlign: 'center'
+    },
+    details: {
 
-  getStyle4: function () {
+      bottom: -100,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'center'
+    }
+  },
+  {
+    titleText: {
+      top: 45,
+      left: -75
+    },
+    userText: {
+      textAlign: 'left',
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      left: -94,
+      top: 50,
+      fontSize: 15,
+      textAlign: 'left'
 
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 65
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 75,
-          fontSize: 15,
-          color: 'white',
-          textAlign: 'center'
-        },
-        details: {
-          right: -90,
-          top: -24,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+    },
+    details: {
+      right: -85,
+      bottom: -85,
+      fontSize: 10,
+      textAlign: 'right'
+    }
   },
-  getStyle5: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 65,
-          left: -60
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          left: -78,
-          top: 70,
-          fontSize: 15,
-          color: 'white',
-
-        },
-        details: {
-          right: -70,
-          bottom: -80,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      })
-    );
+  {
+    titleText: {
+      top: 20
+    },
+    userText: {
+      color: 'maroon',
+      fontSize: 25,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      left: -90,
+      bottom: -119,
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+      justifyContent: 'center',
+      textAlign: 'right'
+    },
+    details: {
+      right: -85,
+      bottom: -95,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'right'
+    }
   },
-  getStyle6: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 55,
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 60,
-          fontSize: 15,
-          color: 'white',
-          textAlign: 'center'
-        },
-        details: {
-
-          bottom: -100,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'center'
-        }
-      })
-    );
+  {
+    titleText: {
+      top: 55,
+      left: -50
+    },
+    userText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 60,
+      left: -55,
+      fontSize: 15,
+      color: 'white',
+      textAlign: 'center'
+    },
+    details: {
+      left: -55,
+      bottom: -100,
+      fontSize: 10,
+      color: 'white',
+      textAlign: 'center'
+    }
   },
-  getStyle7: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 45,
-          left: -75
-        },
-        userText: {
-          textAlign: 'left',
-          fontSize: 20,
-          fontWeight: 'bold'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          left: -94,
-          top: 50,
-          fontSize: 15,
-          textAlign: 'left'
-
-        },
-        details: {
-          right: -85,
-          bottom: -85,
-          fontSize: 10,
-          textAlign: 'right'
-        }
-      })
-    );
-  },
-
-  getStyle8: function () {
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 20
-        },
-        userText: {
-          color: 'maroon',
-          fontSize: 25,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          left: -90,
-          bottom: -119,
-          fontSize: 20,
-          fontWeight: 'bold',
-          color: 'white',
-          justifyContent: 'center',
-          textAlign: 'right'
-        },
-        details: {
-          right: -85,
-          bottom: -95,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'right'
-        }
-      }));
-  },
-  getStyle9: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 55,
-          left: -50
-        },
-        userText: {
-          color: 'white',
-          fontSize: 20,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 60,
-          left: -55,
-          fontSize: 15,
-          color: 'white',
-          textAlign: 'center'
-        },
-        details: {
-          left: -55,
-          bottom: -100,
-          fontSize: 10,
-          color: 'white',
-          textAlign: 'center'
-        }
-      })
-    );
-  },
-  getStyle10: function () {
-    // add borders
-    return (
-      StyleSheet.create({
-        titleText: {
-          top: 55,
-          left: -15
-        },
-        userText: {
-          color: 'lightskyblue',
-          fontSize: 20,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        },
-        user: {
-          alignItems: 'center',
-          justifyContent: 'center'
-        },
-        company: {
-          fontWeight: 'bold',
-          top: 60,
-          left: -15,
-          fontSize: 15,
-          color: 'lightskyblue',
-          textAlign: 'center'
-        },
-        details: {
-          left: -80,
-          bottom: -120,
-          fontSize: 10,
-          color: 'lightskyblue',
-          textAlign: 'left'
-        }
-      })
-    );
-  },
-}
+  {
+    titleText: {
+      top: 55,
+      left: -15
+    },
+    userText: {
+      color: 'lightskyblue',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    user: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    company: {
+      fontWeight: 'bold',
+      top: 60,
+      left: -15,
+      fontSize: 15,
+      color: 'lightskyblue',
+      textAlign: 'center'
+    },
+    details: {
+      left: -80,
+      bottom: -120,
+      fontSize: 10,
+      color: 'lightskyblue',
+      textAlign: 'left'
+    }
+  }
+];
 
 export default templateStyles;
