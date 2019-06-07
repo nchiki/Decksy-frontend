6392565c9446c75f8502a930530f388df95ced05
Lloyd <lloyd.clowes@gmail.com>
2019-06-07 18:17:58 +0100

    Add swipe options for HomeScreen

--- components/ContactCollection.js
+++ components/ContactCollection.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { FlatList, StyleSheet, ImageBackground, TouchableOpacity, Text, Image, View } from 'react-native';
 import apiRequests from '../api_wrappers/BackendWrapper';
+import Swipeout from 'react-native-swipeout';
 
 import templateUtils from './Templates';
-
 
 export default class ContactCollection extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      _isMounted: false,
       details: null,
     }
+  }
+
+  componentWillMount() {
+    this.setState({
+      details: this.props.contacts
+    });
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -22,42 +27,40 @@ export default class ContactCollection extends React.Component {
     }
   };
 
-
   handleCardProfile = (item) => {
     this.props.navigation.navigate('CardProfile', { item: item });
   }
 
-
-  _getContact = ({ item }) => (
-    <View style={{ height: 120, flexDirection: 'row', alignItems: 'center' }}>
-      <View style={{ flex: 1, alignItems: 'flex-start', marginLeft: 16 }}>
-        <Text style={{ fontSize: 18 }}>{`${item.firstName} ${item.lastName}`}</Text>
-        <Text style={{ fontSize: 13 }}>{item.profession}</Text>
-      </View>
-      <View style={{ flex: 3, marginRight: -70 }}>
-        <TouchableOpacity style={styles.card} onPress={() => this.handleCardProfile(item)}>
-          <ImageBackground source={templateUtils.setImage(item.card)} style={styles.containerStyle}>
-            <View style={styles.containerStyle}>
-              <View style={templateUtils.setStyle(item.card).titleText}>
-                <Text style={templateUtils.setStyle(item.card).userText} >{`${item.firstName} ${item.lastName}`} </Text>
+  renderRow = ({ item }) => (
+    <Swipeout
+      left={this.leftSwipeButtons}
+      right={this.rightSwipeButtons}
+      autoClose={true}
+      backgroundColor='transparent'
+    >
+      <View style={{ height: 120, flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'flex-start', marginLeft: 16 }}>
+          <Text style={{ fontSize: 18 }}>{`${item.firstName} ${item.lastName}`}</Text>
+          <Text style={{ fontSize: 13 }}>{item.profession}</Text>
+        </View>
+        <View style={{ flex: 3, marginRight: -70 }}>
+          <TouchableOpacity style={styles.card} onPress={() => this.handleCardProfile(item)}>
+            <ImageBackground source={templateUtils.setImage(item.card)} style={styles.containerStyle}>
+              <View style={styles.containerStyle}>
+                <View style={templateUtils.setStyle(item.card).titleText}>
+                  <Text style={templateUtils.setStyle(item.card).userText} >{`${item.firstName} ${item.lastName}`} </Text>
+                </View>
+                <View style={templateUtils.setStyle(item.card).user}>
+                  <Text style={templateUtils.setStyle(item.card).company}>{item.company}</Text>
+                  <Text style={templateUtils.setStyle(item.card).details}>{item.phoneNumber}{'\n'}{item.email}</Text>
+                </View>
               </View>
-              <View style={templateUtils.setStyle(item.card).user}>
-                <Text style={templateUtils.setStyle(item.card).company}>{item.company}</Text>
-                <Text style={templateUtils.setStyle(item.card).details}>{item.phoneNumber}{'\n'}{item.email}</Text>
-              </View>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
+            </ImageBackground>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </Swipeout>
   );
-
-  componentWillMount() {
-    this.setState({
-      _isMounted: true,
-      details: this.props.contacts
-    });
-  }
 
   renderFlatList = () => {
     console.log(item);
@@ -69,7 +72,7 @@ export default class ContactCollection extends React.Component {
           {
             users.map((u, i) => {
               return (
-                this._getContact(u, i),
+                this.renderRow(u, i),
                 this.renderSeparator()
               );
             })
@@ -79,10 +82,6 @@ export default class ContactCollection extends React.Component {
     } else {
       return null;
     }
-  }
-
-  componentWillUnmount() {
-    this.setState({ _isMounted: false });
   }
 
   renderSeparator = () => {
@@ -96,18 +95,54 @@ export default class ContactCollection extends React.Component {
     );
   };
 
-  render() {
+  deleteCard() {
+    alert("TODO");
+  }
 
+  pinCard() {
+    alert("TODO");
+  }
+
+  archiveCard() {
+    alert("TODO");
+  }
+
+  render() {
     return (
       <FlatList
         data={this.props.contacts}
-        renderItem={this._getContact}
+        renderItem={this.renderRow}
         keyExtractor={item => item.email}
         ItemSeparatorComponent={this.renderSeparator}
         style={{ marginTop: -2 }}
       />
     );
   }
+
+  rightSwipeButtons = [
+    {
+      text: 'Archive',
+      backgroundColor: 'dodgerblue',
+      underlayColor: 'rgba(255, 255, 255, 1.0)',
+      onPress: () => { this.archiveCard() }
+    },
+    {
+      text: 'Delete',
+      backgroundColor: 'red',
+      underlayColor: 'rgba(255, 255, 255, 1.0)',
+      onPress: () => { this.deleteCard() }
+    },
+  ];
+
+  leftSwipeButtons = [
+    {
+      text: 'Pin',
+      backgroundColor: 'orange',
+      underlayColor: 'rgba(255, 255, 255, 1.0)',
+      onPress: () => { this.pinCard() }
+    },
+  ];
+
 }
 
 const styles = StyleSheet.create({
